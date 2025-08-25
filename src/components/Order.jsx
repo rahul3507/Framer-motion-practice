d973a87a6101b743b2e355892a52f28b0df873b4
RAHUL MALLIK <rmallik191242@bscse.uiu.ac.bd>
2025-08-24 23:07:11 -0600

    Refactor Order component to include animation variants for improved visual effects

--- src/components/Order.jsx
+++ src/components/Order.jsx
@@ -19,7 +19,12 @@ const containerVarient = {
 
 const Order = ({ pizza }) => {
   return (
-    <motion.div className="container order">
+    <motion.div
+      className="container order"
+      variants={containerVarient}
+      initial="hidden"
+      animate="visible"
+    >
       <h2>Thank you for your order :)</h2>
       <p>You ordered a {pizza.base} pizza with:</p>
       {pizza.toppings.map((topping) => (
