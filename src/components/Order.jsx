0fab7562ef2b0c6f94208fffcff2afd81b99ce40
RAHUL MALLIK <rmallik191242@bscse.uiu.ac.bd>
2025-08-24 23:09:25 -0600

    Wrap toppings list in motion.div for enhanced animation support

--- src/components/Order.jsx
+++ src/components/Order.jsx
@@ -27,9 +27,11 @@ const Order = ({ pizza }) => {
     >
       <h2>Thank you for your order :)</h2>
       <p>You ordered a {pizza.base} pizza with:</p>
-      {pizza.toppings.map((topping) => (
-        <div key={topping}>{topping}</div>
-      ))}
+      <motion.div>
+        {pizza.toppings.map((topping) => (
+          <div key={topping}>{topping}</div>
+        ))}
+      </motion.div>
     </motion.div>
   );
 };
